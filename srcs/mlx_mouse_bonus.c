220f1d0c56da5073a991a9596c586dd5c6fad1b7
Volodya Ismailyan <nenie_iri@mail.ru>
2022-01-17 23:47:29 +0400

    mouse part backup

--- srcs/mlx_mouse_bonus.c
+++ srcs/mlx_mouse_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 22:36:48 by vismaily          #+#    #+#             */
-/*   Updated: 2022/01/15 23:58:45 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/01/17 23:37:22 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,9 @@
 
 int	mlx_mouse(int x, int y, void *param)
 {
-	int	x_cord;
-	int	y_cord;
-
 	(void)param;
 	(void)y;
-	x_cord = -99999;
-	y_cord = -99999;
-	mlx_mouse_get_pos(game.mlx, game.mlx_win, &x_cord, &y_cord);
-	if (game.draw.motion_x != 99999 && x_cord >= 0 && y_cord >= 0 && \
-			x_cord < game.map.r_x && y_cord < game.map.r_y)
+	if (game.draw.motion_x != 99999)
 	{
 		if (game.draw.motion_x > x)
 			mlx_press(KEY_ARROW_LEFT);
